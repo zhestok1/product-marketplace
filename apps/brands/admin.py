e211647e9776811b99ca3c5a65c9bcd3f216ba7f
zhestok1 <rp3truhin@yandex.ru>
2026-08-17 12:31:47 +0400

    add brand model and admin for brand model

--- apps/brands/admin.py
+++ apps/brands/admin.py
@@ -1,3 +1,46 @@
+from django.contrib.auth import get_user
 from django.contrib import admin
 
-# Register your models here.
+from .models import BrandProfile, OOODetails, IPSEDetails
+
+user = get_user()
+
+class OOODetailsInline(admin.TabularInline):
+    model = OOODetails
+    extra = 1
+    
+    
+class IPSEDetailsInline(admin.TabularInline):
+    model = IPSEDetails
+    extra = 1
+    
+@admin.register(BrandProfile)
+class BrandProfileAdmin(admin.ModelAdmin):
+    
+    list_display = ['user', 'brand_type', 'name', 'inn', 'ogrn' , 'created_at']
+    list_filter = ['created_at', 'brand_type']
+    readonly_fields = ['created_at']
+    
+    fieldsets = [
+        ('Основная информация об организации', {'fields': ('user', 'brand_type', 
+                                                           'name', 'inn', 
+                                                           'ogrn' , 'created_at', )})
+    ]
+    
+    add_fieldsets = [
+        ('Основаная информация об организации', {
+            'fields': ('user', 'brand_type', 'name', 'inn', 'ogrn', )
+        })
+    ]
+    
+    def get_inline_instances(self, request, obj = None):
+        inline_instances = []
+        
+        if obj is not None:
+            if obj.brand_type == 'OOO':
+                inline_instances.append(OOODetailsInline(self.model, self.admin_site))
+            else:
+                inline_instances.append(IPSEDetails(self.model, self.admin_site))
+            
+
+
--- apps/brands/models.py
+++ apps/brands/models.py
@@ -1,3 +1,65 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+class BrandProfile(models.Model):
+    class BrandType(models.TextChoices):
+        SELF_EMPLOYED = 'SE', 'Самозанятый'
+        INDIVIDUAL_ENTREPRENEUR = 'IP', 'ИП'
+        OOO = 'OOO', 'ООО'
+    
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name='brands'
+    )
+    brand_type = models.CharField(
+        max_length=3,  
+        choices=BrandType.choices,
+    )
+
+    name = models.CharField(max_length=255, verbose_name='Название бренда')
+    inn = models.CharField(max_length=12, unique=True, verbose_name='ИНН')
+    ogrn = models.CharField(max_length=15, blank=True, null=True, verbose_name='ОГРН/ОГРНИП')
+    logo = models.ImageField(upload_to='logos/', blank=True, null=True) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = 'Компания'
+        verbose_name_plural = 'Компании'
+        ordering = ['-created_at']
+        
+    def __str__(self):
+        return f"{self.get_brand_type_display()} «{self.name}» (ИНН: {self.inn})"
+
+
+class OOODetails(models.Model):
+    brand = models.OneToOneField(
+        BrandProfile, 
+        on_delete=models.CASCADE, 
+        related_name='ooo_details'
+    )
+    full_name = models.CharField(max_length=300, verbose_name='Полное наименование организации')
+    kpp = models.CharField(max_length=9, verbose_name='КПП')
+    legal_address = models.TextField(verbose_name='Юридический адрес')
+
+    class Meta:
+        verbose_name = 'Реквизиты ООО'
+        verbose_name_plural = 'Реквизиты ООО'
+
+
+class IPSEDetails(models.Model):  
+    brand = models.OneToOneField(
+        BrandProfile, 
+        on_delete=models.CASCADE, 
+        related_name='ipse_details'
+    )
+    first_name = models.CharField(max_length=300, verbose_name='Имя')
+    last_name = models.CharField(max_length=300, verbose_name='Фамилия')
+    
+    passport_data = models.TextField(verbose_name='Паспортные данные')
+
+    class Meta:
+        verbose_name = 'Реквизиты ИП/Самозанятого'
+        verbose_name_plural = 'Реквизиты ИП/Самозанятых'
